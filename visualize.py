122232009b59ef634da476ea8dfbec1db88df3d3
Yuze.Ma <584653629@qq.com>
2017-09-04 16:30:29 -0400

    Extract key words of a company

--- visualize.py
+++ visualize.py
@@ -1,13 +1,26 @@
+# -*- encoding: utf-8 -*-
+from __future__ import print_function, unicode_literals
+from bosonnlp import BosonNLP
 from pymongo import *
 import pprint
 
-def printResult(_cursor, _attribution=None):
+
+nlp = BosonNLP('CIeQWQuR.15234.TVGB35Uq5nuk')
+
+
+
+
+
+def decodeResult(_cursor, _attribution=None):
+    results = []
     if(_attribution):
         for item in _cursor:
-            pprint.pprint(item[_attribution])
-        return
+            results.append(item[_attribution])
+            # pprint.pprint(item[_attribution])
+        return results
     for item in _cursor:
         pprint.pprint(item)
+    return
 
 # Connect to mongodb
 client = MongoClient("mongodb://localhost:27017/")
@@ -17,9 +30,18 @@ collection = db.lagouJob
 
 
 # Find by company name
-cursor = collection.find({"companyName":"通联数据股份公司"})
+cursor = collection.find({"companyName":"深圳市贝利美维软件有限公司"})
 
 # Find by Job Title
-cursor = collection.find({"jobTitle":{"$regex": u"区块"}})
-printResult(cursor, "jobDescription")
+cursor = collection.find({"jobTitle":{"$regex": u"区块链"}})
+result = decodeResult(cursor, "jobDescription")
+resultContent = "".join(result)
+nlpResult = nlp.extract_keywords(resultContent, top_k=100)
+
+f = open('data.txt', 'w')
+for weight, word in nlpResult:
+    outString = str(weight) + " "+ str(word) + "\n"
+    print(weight, word)
+    f.write(outString)
+f.close() 
 
